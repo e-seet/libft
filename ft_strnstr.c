18a342ec426859bc2734e1d17073c86064334a8a
2202210 <2202210@sit.singaporetech.edu.sg>
2023-09-08 15:33:13 +0800

    push strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -21,40 +21,114 @@
 //if little occurs nowhere in big, NULL is returned; 
 //otherwise a pointer to the first character of the first occurrence of little is returned.
 
+// RETURN VALUES
+//      If needle is an empty string, haystack is returned; if needle occurs
+//      nowhere in haystack, NULL is returned; otherwise a pointer to the first
+//      character of the first occurrence of needle is returned.
 
+// EXAMPLES
+//      The following sets the pointer ptr to the "Bar Baz" portion of
+//      largestring:
 
+//            const char *largestring = "Foo Bar Baz";
+//            const char *smallstring = "Bar";
+//            char *ptr;
+
+//            ptr = strstr(largestring, smallstring);
+
+//      The following sets the pointer ptr to NULL, because only the first 4
+//      characters of largestring are searched:
+
+//            const char *largestring = "Foo Bar Baz";
+//            const char *smallstring = "Bar";
+//            char *ptr;
+
+//            ptr = strnstr(largestring, smallstring, 4);
+
+// char * strnstr(const char *haystack, const char *needle, size_t len);
+#include <stddef.h>
+
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    size_t  i;
+    size_t  j;
+    char    *str;
+    char    *find;
+
+    str = (char *) haystack;
+    find = (char *) needle;
+    i = 0;
+    while (str[i] != '\0' && len > i)
+    {
+        j = 0;
+        if(str[i] == needle[j])
+        {
+            while (str[i + j] != '\0' && find[j] != '\0' && len > i + j)
+                j++;
+            if (find[j] == '\0')
+                return &str[i];
+        }
+        i++;        
+    }
+
+    return NULL;
+}
 
 
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-    // Test Case 1: Concatenate two strings with a specified maximum length
-    // Test Case 1: Concatenation with max length 5: "Hello, World"
+    // Test case 1: Search for a substring present in the string within the specified length
+    const char *haystack1 = "Hello, World!";
+    const char *needle1 = "World";
+    size_t max_length1 = 12;  // Limit the search to the first 12 characters
+    char *result1 = strnstr(haystack1, needle1, max_length1);
 
-    char dest1[20] = "Hello, ";
-    const char *src1 = "World!";
-    size_t max_length1 = 5;
-    
-    strncat(dest1, src1, max_length1);
-    printf("Test Case 1: Concatenation with max length %zu: \"%s\"\n", max_length1, dest1);
+    if (result1 != NULL) {
+        printf("Test case 1 passed. Found substring: %s\n", result1);
+    } else {
+        printf("Test case 1 failed. Substring not found.\n");
+    }
 
-    // Test Case 2: Concatenate two strings with a longer maximum length
-    // Test Case 2: Concatenation with max length 10: "Hello, World!"
-    char dest2[20] = "Hello, ";
-    const char *src2 = "World!";
-    size_t max_length2 = 10;
+    // Test case 2: Search for a substring present in the string within the specified length
+    // Test case fail.
+    const char *haystack2 = "Hello, World!";
+    const char *needle2 = "World";
+    size_t max_length2 = 10;  // Limit the search to the first 10 characters
+    char *result2 = strnstr(haystack2, needle2, max_length2);
 
-    strncat(dest2, src2, max_length2);
-    printf("Test Case 2: Concatenation with max length %zu: \"%s\"\n", max_length2, dest2);
+    if (result2 != NULL) {
+        printf("Test case 2 passed. Found substring: %s\n", result2);
+    } else {
+        printf("Test case 2 failed. Substring not found.\n");
+    }
+
+    // Test case 3: Search for a substring not present in the string within the specified length
+    // fail as it does not exist
+    const char *haystack3 = "This is a test.";
+    const char *needle3 = "foobar";
+    size_t max_length3 = 15;  // Limit the search to the first 15 characters
+    char *result3 = strnstr(haystack3, needle3, max_length3);
+
+    if (result3 == NULL) {
+        printf("Test case 3 passed. Substring not found.\n");
+    } else {
+        printf("Test case3  failed. Found unexpected substring: %s\n", result2);
+    }
+
+    // Test case 4:
+    const char *haystack4 = "Hello, World!";
+    const char *needle4 = "ell";
+    size_t max_length4 = 10;  // Limit the search to the first 12 characters
+    char *result4 = strnstr(haystack4, needle4, max_length4);
+
+    if (result4 != NULL) {
+        printf("Test case 4 passed. Found substring: %s\n", result4);
+    } else {
+        printf("Test case 4 failed. Substring not found.\n");
+    }
 
 
-
-	const char *largestring = "Foo Bar Baz";
-	const char *smallstring = "Bar";
-	char *ptr;
-	ptr = strnstr(largestring, smallstring, 4);
-
-	printf("%s\n", ptr);
     return 0;
 }
